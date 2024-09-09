032104cde2cb65ff23668ddd8905c53fc7ba1f71
Johan le Roux <johan@munch.cloud>
2024-09-09 09:24:56 +0200

    Initial i3 commit

---
+++ install-rofi-network-manager.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone --depth 1 --branch master https://github.com/P3rf/rofi-network-manager.git
+cd rofi-network-manager
+./src/ronema
+./setup.sh install
---
+++ screenlayout/.screenlayout/temp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 2880x1800 --pos 0x1080 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --mode 1920x1080 --pos 409x0 --rotate normal --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off --output DisplayPort-8 --off --output DP-1-1 --off --output HDMI-1-1 --off --output eDP-1-1 --off

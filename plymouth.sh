2e2d849b4992ec00c6b158e9f8c29d7bdf1398d6
Johan le Roux <jleroux99@gmail.com>
2025-11-19 18:54:38 +0200

    wip

--- plymouth.sh
+++ plymouth.sh
@@ -21,22 +21,38 @@ if [[ ! -f $limine_config ]]; then
     exit 1
 fi
 
-NEW_HOOKS="base udev plymouth autodetect microcode modconf kms keyboard keymap consolefont block encrypt filesystems fsck"
-CONF="/etc/mkinitcpio.conf"
+  CMDLINE=$(grep "^[[:space:]]*cmdline:" "$limine_config" | head -1 | sed 's/^[[:space:]]*cmdline:[[:space:]]*//')
 
-echo "Updating HOOKS in $CONF"
-sudo sed -i -E "s|^HOOKS=.*$|HOOKS=(${NEW_HOOKS})|" "$CONF"
+  sudo tee /etc/default/limine <<EOF >/dev/null
+TARGET_OS_NAME="Custom Arch Linux"
 
-# Check if the theme is not already set
-current_theme=$(plymouth-set-default-theme)
-if [[ "$current_theme" != "arch-mac-style" ]]; then
-    echo "Selecting Plymouth theme"
-    sudo cp -r ./assets/arch-mac-style /usr/share/plymouth/themes/
-    sudo plymouth-set-default-theme -R arch-mac-style
+ESP_PATH="/boot"
+
+KERNEL_CMDLINE[default]="$CMDLINE"
+KERNEL_CMDLINE[default]+="quiet splash"
+
+ENABLE_UKI=yes
+CUSTOM_UKI_NAME="arch"
+
+ENABLE_LIMINE_FALLBACK=yes
+
+# Find and add other bootloaders
+FIND_BOOTLOADERS=yes
+
+BOOT_ORDER="*, *fallback, Snapshots"
+
+MAX_SNAPSHOT_ENTRIES=5
+
+SNAPSHOT_FORMAT_CHOICE=5
+EOF
+
+# UKI and EFI fallback are EFI only
+if [[ -z $EFI ]]; then
+    sudo sed -i '/^ENABLE_UKI=/d; /^ENABLE_LIMINE_FALLBACK=/d' /etc/default/limine
 fi
 
-  # We overwrite the whole thing knowing the limine-update will add the entries for us
-  sudo tee /boot/limine.conf <<EOF >/dev/null
+# We overwrite the whole thing knowing the limine-update will add the entries for us
+sudo tee /boot/limine.conf <<EOF >/dev/null
 ### Read more at config document: https://github.com/limine-bootloader/limine/blob/trunk/CONFIG.md
 #timeout: 3
 default_entry: 2
@@ -53,6 +69,25 @@ term_foreground_bright: cad3f5
 
 EOF
 
+# Remove the original config file if it's not /boot/limine.conf
+if [[ "$limine_config" != "/boot/limine.conf" ]] && [[ -f "$limine_config" ]]; then
+    sudo rm "$limine_config"
+fi
+
 echo "Regenerating initramfs"
 sudo limine-update
 
+
+NEW_HOOKS="base udev plymouth autodetect microcode modconf kms keyboard keymap consolefont block encrypt filesystems fsck"
+CONF="/etc/mkinitcpio.conf"
+
+echo "Updating HOOKS in $CONF"
+sudo sed -i -E "s|^HOOKS=.*$|HOOKS=(${NEW_HOOKS})|" "$CONF"
+
+# Check if the theme is not already set
+current_theme=$(plymouth-set-default-theme)
+if [[ "$current_theme" != "arch-mac-style" ]]; then
+    echo "Selecting Plymouth theme"
+    sudo cp -r ./assets/arch-mac-style /usr/share/plymouth/themes/
+    sudo plymouth-set-default-theme -R arch-mac-style
+fi

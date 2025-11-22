4a071f5cbefc932e1641e9c672098f914d30a698
Johan le Roux <jleroux99@gmail.com>
2025-11-22 20:40:08 +0200

    wip

--- install/config/git.sh
+++ install/config/git.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 echo "Configuring Git..."
-read -p "Enter your name: " git_name
-read -p "Enter your email: " git_email
 
-git config --global user.name "$git_name"
-git config --global user.email "$git_email"
+# Check if user name has not been set
+git_name=$(git config --global user.name || echo "")
+if [ -z "$git_name" ]; then
+  read -p "Enter your name: " git_name
+  git config --global user.name "$git_name"
+fi
+
+# Check if user email has not been set
+git_email=$(git config --global user.email || echo "")
+if [ -z "$git_email" ]; then
+  read -p "Enter your email: " git_email
+  git config --global user.email "$git_email"
+fi

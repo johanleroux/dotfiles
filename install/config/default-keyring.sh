60687f2f2b64a2bac30c741a8bc5080ce3461a74
Johan le Roux <jleroux99@gmail.com>
2026-01-04 16:09:30 +0200

    wip

--- install/config/default-keyring.sh
+++ install/config/default-keyring.sh
@@ -6,6 +6,13 @@ DEFAULT_FILE="$KEYRING_DIR/default"
 
 mkdir -p $KEYRING_DIR
 
+# Create default keyring file if it doesn't exist
+if [ -f "$KEYRING_FILE" ] && [ -f "$DEFAULT_FILE" ]; then
+  echo "Default keyring already exists. Skipping creation."
+  exit 0
+fi
+
+echo "Creating default keyring..."
 cat << EOF | tee "$KEYRING_FILE"
 [keyring]
 display-name=Default keyring
@@ -15,6 +22,7 @@ lock-on-idle=false
 lock-after=false
 EOF
 
+echo "Setting default keyring..."
 cat << EOF | tee "$DEFAULT_FILE"
 Default_keyring
 EOF

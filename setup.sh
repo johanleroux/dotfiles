e339b76d30255326d3c475a1a81df45e65c9ae79
Johan le Roux <jleroux99@gmail.com>
2026-01-07 13:39:45 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -84,6 +84,11 @@ fi
 if ! [ -d "$HOME/.nvm" ]; then
   echo "Installing nvm"
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+  echo "Installing latest LTS version of Node.js"
   nvm install --lts
   nvm use --lts
 fi
